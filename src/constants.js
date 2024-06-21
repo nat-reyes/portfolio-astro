a10e4e19c19952ecdef4bfdfb3fea43efb91479d
Nat <natalia.reyes@dowjones.com>
2024-06-21 17:16:04 -0500

    Updated experience, styles and accesibility

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,8 @@
-
+import dreamiImg from "../public/dreami.jpeg";
+import logoUfpsImg from "../public/logoufps.png";
+import courseraImg from "../public/coursera.webp";
+import platziImg from "../public/platzi.png";
+import deeplearningImg from "../public/deeplearning.png";
 
 
 export const TAGS = {
@@ -31,42 +35,42 @@ export const education = [
     {
         title: "Software Engineer",
         certifiedBy: "University Francisco de Paula Santander",
-        certifiedByIcon: "../../public/logoufps.png",
+        certifiedByIcon: logoUfpsImg,
         tags: [TAGS.NEXT, TAGS.COMPUTER_SCIENCE],
     },
     {
         title: "Software Design and architecture",
         certifiedBy: "Coursera (University of Alberta)",
         link: "https://www.coursera.org/account/accomplishments/certificate/E85RVDBCR2AY",
-        certifiedByIcon: "../../public/coursera.webp",
+        certifiedByIcon: courseraImg,
         tags: [TAGS.COMPUTER_SCIENCE, TAGS.JAVASCRIPT],
     },
     {
         title: "Machine Learning and Big Data",
         certifiedBy: "Coursera",
         link: "https://www.coursera.org/account/accomplishments/verify/3QG25YQKUM7G",
-        certifiedByIcon: "../../public/coursera.webp",
+        certifiedByIcon: courseraImg,
         tags: [TAGS.PYTHON],
     },
     {
         title: "React hooks patterns design and composition",
         certifiedBy: "Platzi",
         link: "https://platzi.com/p/natrv1/curso/2457-react-patrones-render/diploma/detalle/,",
-        certifiedByIcon: "../../public/platzi.png",
+        certifiedByIcon: platziImg,
         tags: [TAGS.REACT, TAGS.JAVASCRIPT],
     },
     {
         title: "Next security web  with OWASP",
         certifiedBy: "Platzi",
         link: "https://platzi.com/p/natrv1/curso/2429-nextjs-owasp/diploma/detalle/",
-        certifiedByIcon: "../../public/platzi.png",
+        certifiedByIcon: platziImg,
         tags: [TAGS.NEXT, TAGS.JAVASCRIPT],
     },
     {
         title: "Introduction to CrewAI",
         certifiedBy: "DeepLearning.ai",
         link: "https://learn.deeplearning.ai/accomplishments/879ee54f-3d8d-4828-a04d-f204b52e7e3c",
-        certifiedByIcon: "../../public/deeplearning.png",
+        certifiedByIcon: deeplearningImg,
         tags: [TAGS.CREWAI],
     }
 ]
@@ -76,7 +80,7 @@ export const volunteering = [
         title: "Mentor",
         certifiedBy: "Dreami",
         link: "https://www.linkedin.com/company/dreami-io/posts/?feedView=all",
-        certifiedByIcon: "../../public/dreami.jpeg",
+        certifiedByIcon: dreamiImg ,
         tags: [],
     }
 ]
