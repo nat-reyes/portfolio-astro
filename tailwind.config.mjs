3ff36fddd89e03740ee13f78f0ce82c9dd0d36f2
Nat <natalia.reyes@dowjones.com>
2024-07-04 17:52:30 -0500

    latest changes mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			'xs': '375px',
+			...defaultTheme.screens,
+		},
 	},
 	plugins: [],
 }
